d1380e462d1c01c662a7245732ed95c4a8dd19d1
César Vargas <vargas.cesar88@gmail.com>
2025-01-09 19:44:48 -0800

    Initial Commit

---
+++ Sensores/SensoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensoresApp.swift
+//  Sensores
+//
+//  Created by Cesar Vargas on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SensoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
